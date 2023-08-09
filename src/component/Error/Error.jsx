e316bda6d18c432750584f954a755a52d9457d45
Prakash Chand <Prakash0719pc@gmail.com>
2023-08-09 15:27:21 +0530

    update

--- src/component/Error/Error.jsx
+++ src/component/Error/Error.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import Navbar from '../Navbar/Navbar';
 import './Error.css';
 const Error = () => {
   return (
     <>
-      <Navbar/>
       <div className='error_img'>
         <img src={require('../img/error.webp')} style={{marginLeft:'280px'}}/>
       </div>
